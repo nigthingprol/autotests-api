6750dcd3ba579ecc62be7998b8296842d6598e75
nigthingprol <nalivayko_2018@inbox.ru>
2025-12-22 18:52:45 +0700

    Фикстуры Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 import pytest
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from http import HTTPStatus
@@ -8,11 +8,10 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-    public_users_client = get_public_users_client()
-
+def test_create_user(public_users_client: PublicUsersClient):
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
+    
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
