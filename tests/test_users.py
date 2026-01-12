8640c2fa5f74ecec308c43b96a684ec29ce29866
nigthingprol <nalivayko_2018@inbox.ru>
2026-01-12 19:54:07 +0700

    Перезапуски автотестов в Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,12 +8,14 @@ from fixtures.users import UserFixture
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.users_schema import GetUserResponseSchema
+from tools.fakers import fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
